8ec28e34160e812ec1dade0bcd8887cd59c8e72f
Your Name <you@example.com>
2020-10-06 15:10:23 -0400

    Optimize test case

--- tests/dashboard_tests.py
+++ tests/dashboard_tests.py
@@ -2,6 +2,7 @@ import unittest
 import asyncio
 from Dashboard.ui.services import commit_service
 from Dashboard.ui.plot import plot
+import xml
 
 
 class GithubTestCases(unittest.TestCase):
@@ -17,13 +18,17 @@ class GithubTestCases(unittest.TestCase):
     Test commit figure generation.
     """
     def test_commit_table(self):
+        def assert_result(result):
+            # print(result)
+            self.assertTrue(result.find("<?xml") == 0)   # svg
+
         import json
         with open("test_data.json", 'r') as f:
             data = json.loads(f.read())
             commits, most_commits = commit_service.generate_commit_table(data)
 
             f = self.loop.create_task(plot.plot_commits(commits, most_commits))
-            f.add_done_callback(lambda result: print(result.result().getvalue()))
+            f.add_done_callback(lambda result: assert_result(result.result().getvalue()))
 
             self.loop.run_until_complete(f)
 
