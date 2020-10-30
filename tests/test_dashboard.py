f7e54356413f91bb43cae574821b68f476f74d5c
Ji Wang <jeffwji@gmail.com>
2020-10-30 16:08:56 -0400

    Update test to async

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,37 +1,23 @@
 import unittest
-import asyncio
 from ui.services import commit_service
 from ui.plot import plot
+import json
 
 
 """
-Switch to "/h/to/DevOpsDashboard/Dashboard"
+Switch to "/path/to/DevOpsDashboard/Dashboard"
 """
-class GithubTestCases(unittest.TestCase):
-    loop = None
-
-    def setUp(self):
-        self.loop = asyncio.get_event_loop()
-
-    def tearDown(self):
-        self.loop.close()
-
+class GithubTestCases(unittest.IsolatedAsyncioTestCase):
     """
     Test commit figure generation.
     """
-    def test_commit_table(self):
-        def assert_result(result):
-            # print(result)
-            self.assertTrue(result.find("<?xml") == 0)   # svg
-
-        import json
+    async def test_commit_table(self):
         with open("tests/test_data.json", 'r') as f:
             data = json.loads(f.read())
             commits, most_commits = commit_service.generate_commit_table(data)
 
-            f = self.loop.create_task(plot.plot_commits(commits, most_commits))
-            f.add_done_callback(lambda result: assert_result(result.result().getvalue()))
-
-            self.loop.run_until_complete(f)
+            svgIO = await plot.plot_commits(commits, most_commits)
+            svg = svgIO.getvalue()
+            self.assertTrue(svg.find("<?xml") == 0)
 
 
