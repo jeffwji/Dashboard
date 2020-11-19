d2b2c61f84e0b898966e35804ea708f65f5a8aa3
Ji Wang <jeffwji@gmail.com>
2020-11-19 14:45:45 -0500

    Format the code and add more comments

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,27 +1,32 @@
 from unittest.mock import Mock
 import pytest
 import os
-import github
+from ui import views
 
 
-# Mock upstream
-async def __get_commits(org, repo):
-    import json
-    with open("tests/test_data.json", 'r') as f:
-        data = json.loads(f.read())
-        return data
-github.github = Mock()
-github.github.get_commits = __get_commits
-
-
-from ui.views import contribute
-
-
+# Async annotation for Pytest
 @pytest.mark.asyncio
 async def test_github_view():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Dashboard.settings")
 
-    # Mock downstream
+    '''
+    Mock upstream:
+    
+    Replace views.github and its function to Mockup object
+    '''
+    async def __get_commits(org, repo):
+        print("\nUsing mocked __get_commits() function")
+
+        import json
+        with open("tests/test_data.json", 'r') as f:
+            data = json.loads(f.read())
+            return data
+    views.github = Mock()
+    views.github.get_commits = __get_commits
+
+    '''
+    Mock downstream
+    '''
     request = Mock()
     request.method = "POST"
     request.body = """
@@ -30,4 +35,4 @@ async def test_github_view():
         "repository": "zio"
     }"""
 
-    await contribute(request)
+    await views.contribute(request)
